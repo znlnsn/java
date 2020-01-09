2f02d82e84bfd0f1620b7cb3cc201459b5dfa93c
ZhengLanshun <44605710+zhenglanshun@users.noreply.github.com>
2020-01-09 16:40:13 +0800

    Add files via upload

---
+++ red_wine1/src/entity/Profile.java
@@ -0,0 +1,23 @@
+package entity;
+
+//¹«Ë¾¼ò½é
+public class Profile {
+	private int id; // id
+	private String content; // ¼ò½éƒÈÈİ
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
